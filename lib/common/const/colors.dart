20f5f7bf884bc6d85f583a5903d61a3f35eb86a9
bammyu <mbjun3630@naver.com>
2024-08-20 23:17:47 +0900

    flutter-lv2 프로젝트 셋업

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF22A45D);
+const bodyTextColor = Color(0xFF868686);
+const inputBgColor = Color(0xFFFBFBFB);
+const inputBorderColor = Color(0xFFF3F2F2);
