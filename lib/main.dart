198eedfbf0539bb5db08fa9af9df5677609b96e9
bammyu <mbjun3630@naver.com>
2024-12-24 22:44:50 +0900

    로그인 화면 UI 설계

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:actual/common/component/custom_text_form_field.dart';
+import 'package:actual/user/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,22 +13,12 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      home: const Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextFormField(
-              hintText: '이메일을 입력해주세요',
-              onChanged: (String value) {},
-            ),
-            CustomTextFormField(
-              hintText: '비밀번호를 입력해주세요',
-              onChanged: (String value) {},
-              obscureText: true,
-            ),
-          ],
-        ),
+        body: LoginScreen(),
       ),
     );
   }
