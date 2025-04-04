f2780773ed04c1e73fb50605e23ec8ae69df654f
bammyu <mbjun3630@naver.com>
2025-04-04 10:13:28 +0900

    Riverpod Dio Provider 적용

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -4,16 +4,14 @@ import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends StatelessWidget {
+class RestaurantScreen extends ConsumerWidget {
   const RestaurantScreen({super.key});
 
-  Future<List<RestaurantModel>> paginateRestaurant() async {
-    final dio = Dio();
-
-    dio.interceptors.add(CustomInterceptor(storage: storage));
+  Future<List<RestaurantModel>> paginateRestaurant(WidgetRef ref) async {
+    final dio = ref.watch(dioProvider);
 
     final response =
         await RestaurantRepository(dio, baseUrl: 'http://$ip/restaurant')
@@ -23,14 +21,14 @@ class RestaurantScreen extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 16.0,
         ),
         child: FutureBuilder<List<RestaurantModel>>(
-          future: paginateRestaurant(),
+          future: paginateRestaurant(ref),
           builder: (context, snapshot) {
             if (!snapshot.hasData) {
               return const Center(
